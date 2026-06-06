f7426de79d041edf68978ec71b78ac49b84b8f18
Rommel Miñao Tima <rmt.aiautomation@gmail.com>
2026-06-06 21:49:37 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
